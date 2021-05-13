355417a572bd1ee60cf17e7476372757f79a1000
dasWarder <Al.94_94@mail.ru>
2021-05-13 16:22:31 +0300

    Add a structure of the application

---
+++ house-core/house-core-web/src/main/java/com/house/HouseCoreWebApplication.java
@@ -0,0 +1,12 @@
+package com.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCoreWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseCoreWebApplication.class, args);
+    }
+}
